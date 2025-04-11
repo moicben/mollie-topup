7dd079c48884644d9a1645982b17eae64e2a0e42
moicben <benjamain.georges@gmail.com>
2025-04-11 10:31:48 +0700

    New SSL Setup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,9 +8,9 @@ module.exports = {
         DISPLAY: ":11",
         XAUTHORITY: "/root/.Xauthority", // ou le chemin approprié pour l'utilisateur exécutant la session
         PUPPETEER_EXECUTABLE_PATH: "/usr/bin/google-chrome-stable",
-        PORT: 3000,            // plus 443
-      SSL_CERT: "/etc/letsencrypt/live/api.christopeit-sport.fr/fullchain.pem",
-      SSL_KEY:  "/etc/letsencrypt/live/api.christopeit-sport.fr/privkey.pem",
+        PORT: 3000, 
+        SSL_CERT: "/etc/letsencrypt/live/api.christopeit-sport.fr/fullchain.pem",
+        SSL_KEY:  "/etc/letsencrypt/live/api.christopeit-sport.fr/privkey.pem",
       }
     }
   ]
