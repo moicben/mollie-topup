f348449041c70f0e789b8158793c2f6cda13a932
moicben <benjamain.georges@gmail.com>
2025-04-16 10:54:26 +0700

    New Western Structure

--- index.js
+++ index.js
@@ -6,70 +6,45 @@ import https from 'https';
 import googleTopup from './googleTopup.js';
 import googleLogin from './googleLogin.js';
 
-import westernInit from './westernInit.js';
+import westernInit from './westernInit.js'; // handler Express qui met à jour westernSession
 import westernProceedHandler from './westernProceed.js';
 import westernTopup from './westernTopup.js';
 import { westernSession } from './westernSession.js';
 
 const app = express();
-const PORT = process.env.PORT || 443; // Port par défaut pour HTTPS
+const PORT = process.env.PORT || 443;
 
-// Charger les certificats SSL
 const sslOptions = {
   key: fs.readFileSync('/etc/letsencrypt/live/api.christopeit-sport.fr/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/api.christopeit-sport.fr/fullchain.pem')
 };
 
-// Autoriser toutes les origines
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-// Middleware pour parser le JSON
 app.use(express.json());
 
-// Route /
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Bienvenue sur la page d\'accueil ' });
 });
 
-//
-
-//
-
-// Route pour le débogage de Google
 app.post('/google-login', googleLogin);
-
-// Route Topup Google
 app.post('/google-topup', googleTopup);
-
-//
-
-//
-
-
-// Initialisation de Western Union
-
-let westernBrowser, westernPage;
-
-app.post('/western-init', ({ westernBrowser, westernPage } = westernInit))
-
-// Transaction Western Union
-app.post('/western-proceed',  westernProceedHandler(westernSession.browser, westernSession.page));
-
-// Route Topup WesternUnion
 app.post('/western-topup', westernTopup);
 
+// La route d'initialisation appelle directement le handler de westernInit
+app.post('/western-init', westernInit);
+
+// La route pour la transaction utilise les objets contenus dans westernSession
+app.post('/western-proceed', (req, res) => {
+  return westernProceedHandler(westernSession.browser, westernSession.page)(req, res);
+});
 
 
-// Lancer la requête programmée de login toutes les 23 heures
-//scheduleMollieLogin();
 
-//
-
-// Démarrer le serveur HTTPS
 https.createServer(sslOptions, app).listen(PORT, () => {
   console.log(`HTTPS Server is running on https://api.christopeit-sport.fr`);
 });
