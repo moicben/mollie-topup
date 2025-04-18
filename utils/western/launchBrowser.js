c97cbbc9730b0fbe8134c33972ef0aef5eaba6da
moicben <benjamain.georges@gmail.com>
2025-04-18 10:03:44 +0700

    Optimized Browser Launch

--- utils/western/launchBrowser.js
+++ utils/western/launchBrowser.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 const proxyAddress = 'brd.superproxy.io';
 const proxyPort = 33335;
 const proxyUsername = 'brd-customer-hl_07d8ef96-zone-residential_proxy1-country-fr';
-const proxyPassword = 'Cadeau2914!';
+const proxyPassword = 'Cadeau2014!';
 const proxyCertificate =  fs.readFileSync('./utils/proxyCertificate.crt', 'utf8');
 
 export async function launchBrowser() {
