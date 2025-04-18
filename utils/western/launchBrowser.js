f784e931a78c50f65df2e5c4f50932a47c14ba67
moicben <benjamain.georges@gmail.com>
2025-04-18 08:44:03 +0700

    Optimized Browser Launch

--- utils/western/launchBrowser.js
+++ utils/western/launchBrowser.js
@@ -38,48 +38,47 @@ export async function launchBrowser() {
     'Sec-Fetch-Site': 'none',
     'Sec-Fetch-User': '?1',
     'Cache-Control': 'max-age=0',
-    'Referer': 'https://www.westernunion.com/fr/fr/web/user/register',	
   });
 
   // Choisir aléatoirement un User-Agent
-  // const userAgents = [
-  //   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-  //   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
-  //   'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
-  //   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
-  //   'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
-  // ];
-  // const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
-  // await page.setUserAgent(randomUserAgent);
+  const userAgents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
+  ];
+  const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+  await page.setUserAgent(randomUserAgent);
 
-  // Injecter des scripts pour tromper certaines détections (ex: webdriver, plugins, langues, etc.)
-  // await page.evaluateOnNewDocument(() => {
-  //   Object.defineProperty(navigator, 'webdriver', { get: () => false });
-  //   window.chrome = { runtime: {} };
-  //   const originalQuery = window.navigator.permissions.query;
-  //   window.navigator.permissions.query = (parameters) =>
-  //     parameters.name === 'notifications'
-  //       ? Promise.resolve({ state: Notification.permission })
-  //       : originalQuery(parameters);
-  //   Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });
-  //   Object.defineProperty(navigator, 'languages', { get: () => ['fr-FR', 'fr'] });
-  //   const getParameter = WebGLRenderingContext.prototype.getParameter;
-  //   WebGLRenderingContext.prototype.getParameter = function(parameter) {
-  //     if(parameter === 37445) return 'Intel Inc.';
-  //     if(parameter === 37446) return 'Intel Iris OpenGL Engine';
-  //     return getParameter(parameter);
-  //   };
-  //   Object.defineProperty(navigator, 'mediaDevices', {
-  //     get: () => ({
-  //       enumerateDevices: () =>
-  //         Promise.resolve([
-  //           { kind: 'videoinput' },
-  //           { kind: 'audioinput' },
-  //           { kind: 'audiooutput' }
-  //         ])
-  //     })
-  //   });
-  // });
+  Injecter des scripts pour tromper certaines détections (ex: webdriver, plugins, langues, etc.)
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', { get: () => false });
+    window.chrome = { runtime: {} };
+    const originalQuery = window.navigator.permissions.query;
+    window.navigator.permissions.query = (parameters) =>
+      parameters.name === 'notifications'
+        ? Promise.resolve({ state: Notification.permission })
+        : originalQuery(parameters);
+    Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });
+    Object.defineProperty(navigator, 'languages', { get: () => ['fr-FR', 'fr'] });
+    const getParameter = WebGLRenderingContext.prototype.getParameter;
+    WebGLRenderingContext.prototype.getParameter = function(parameter) {
+      if(parameter === 37445) return 'Intel Inc.';
+      if(parameter === 37446) return 'Intel Iris OpenGL Engine';
+      return getParameter(parameter);
+    };
+    Object.defineProperty(navigator, 'mediaDevices', {
+      get: () => ({
+        enumerateDevices: () =>
+          Promise.resolve([
+            { kind: 'videoinput' },
+            { kind: 'audioinput' },
+            { kind: 'audiooutput' }
+          ])
+      })
+    });
+  });
 
   return { browser, page };
 }
