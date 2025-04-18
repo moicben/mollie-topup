94e080c09b333d425b331ae120b3ce8134086aaf
moicben <benjamain.georges@gmail.com>
2025-04-18 10:17:47 +0700

    Optimized Browser Launch

--- westernInit.js
+++ westernInit.js
@@ -10,7 +10,7 @@ import { westernSession } from './westernSession.js';
 import { storeWestern } from './utils/supabase/storeWestern.js';
 import { launchBrowser } from './utils/western/launchBrowser.js';
 
-const START_URL = 'https://www.westernunion.com/fr/fr/web/user/register';
+const START_URL = 'https://www.westernunion.com/fr/fr/home.html';
 //const START_URL = 'https://binlist.net/';
 
 async function westernInit(orderNumber, amount) {
