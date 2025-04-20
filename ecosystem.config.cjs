543daceaac7fd39731209a9634e85c5efbd0f7c0
moicben <benjamain.georges@gmail.com>
2025-04-20 16:21:34 +0700

    Fix Crash Bug

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,8 +5,7 @@ module.exports = {
     {
       name: "app",
       script: "./index.js",
-      exec_mode: "fork",
-      instances: 1,
+      exec_mode: "cluster",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
