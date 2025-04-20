8155179b9d819408f4ab8688a9c723391e862960
moicben <benjamain.georges@gmail.com>
2025-04-20 17:12:53 +0700

    Fix Crash Bug

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
       script: "./index.js",
       env: {
         NODE_ENV: "production",
-        DISPLAY: ":11",
+        DISPLAY: ":10",
         XAUTHORITY: "/root/.Xauthority", // ou le chemin approprié pour l'utilisateur exécutant la session
         PUPPETEER_EXECUTABLE_PATH: "/usr/bin/google-chrome-stable",
         PUPPETEER_PROFIL_PATH: "/root/chrome-profile/Default",
